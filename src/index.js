2ebcc193cc26bca43deca51d48bdab76be14a6b2
Zach Lloyd <joseph.z.lloyd@gmail.com>
2024-10-25 10:55:27 -0400

    added comment to index.js and formatted template.html using prettier

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
-import {getShipCoords} from "./dom.js";
+import { getShipCoords } from "./dom.js";
 
 console.log("Up and running");
+// This adds an event listener to the start button which collects the player's
+// specified ship coordinates/orientations, analyzes them for any issues (like)
+// overlap, and if there are none, starts the game
 getShipCoords();
