a576966dd9cd6a50508e9f998f44589c75a8739b
Zach Lloyd <joseph.z.lloyd@gmail.com>
2024-10-25 11:07:19 -0400

    added comments to player.js

--- src/player.js
+++ src/player.js
@@ -1,24 +1,39 @@
-import {Gameboard} from "./gameboard.js";
+import { Gameboard } from "./gameboard.js";
 
+/**
+ * Class used to represent a player of the game (whether real or computer) which
+ * stores both the internal and HTML representation of the player's board
+ */
 class Player {
-    constructor(rows, cols, board) {
-        this.gameboard = new Gameboard(rows, cols);
-        this.htmlBoard = board;
-    }
+  constructor(rows, cols, board) {
+    this.gameboard = new Gameboard(rows, cols);
+    this.htmlBoard = board;
+  }
 
-    getGameboard() {
-        return this.gameboard;
-    }
+  /**
+   *
+   * @returns {Gameboard} the internal representation of the player's
+   * gameboard
+   */
+  getGameboard() {
+    return this.gameboard;
+  }
 }
 
+/**
+ * Class used to represent the user and store their board
+ */
 export class RealPlayer extends Player {
-    constructor(rows, cols, board) {
-        super(rows, cols, board);
-    }
+  constructor(rows, cols, board) {
+    super(rows, cols, board);
+  }
 }
 
+/**
+ * Class used to represent the computer and store their board
+ */
 export class ComputerPlayer extends Player {
-    constructor(rows, cols, board) {
-        super(rows, cols, board);
-    }
+  constructor(rows, cols, board) {
+    super(rows, cols, board);
+  }
 }
